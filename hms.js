5dc7a8da7dc0fae88fbfe6e18f2fcbada59bf49d
joonrb <56960550+joonrb@users.noreply.github.com>
2022-09-21 21:22:51 +0900

    Update hms.js

--- hms.js
+++ hms.js
@@ -1,12 +1,17 @@
+// Javascript page for the all pages
+
+// Function to popup a purchase window when purchase button is clicked
 function popUp(){
     var purchaseWindow = window.open("purchaseWindow.html","", "width = 200", "height=150");
 }
 
+// Function that will alert thank you message and will close the window
 function makePurchase(){
     alert("Thanks for making the Purchase!")
     window.close();
 }
 
+// Function that will update the user review input on the page when submitted
 function reviewSubmit(){
     var text;
     text = document.getElementById("inputReview").value;
